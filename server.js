d0d460c40a57768dc38f15f0a4f49616bce93874
Matthew Oslan <derpthemeus@gmail.com>
2017-04-05 13:41:02 -0400

    Update error handling and logging

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ try {
 }
 var riotAPIKey = process.env.RIOT_API_KEY || secrets.riotApiKey;
 var highscoreDataPath = (process.env.OPENSHIFT_DATA_DIR || "") + "highscoreData.json";
+var LOG_HIGHSCORE_CHANGES = (process.env.LOG_HIGHSCORE_CHANGES === undefined) ? true : (process.env.LOG_HIGHSCORE_CHANGES === "true");
 var champions;
 var championIds;
 var highscores = {};
@@ -151,7 +152,8 @@ function getPlayer(req, res) {
                 getPlayerInfo(req.query.summoner, region).then(function (playerInfo) {
                     if (!playerInfo.redirect) {
                         var player = playerInfo.player;
-                        requestJSON(region.host + "/championmastery/location/" + region.platform + "/player/" + player.id + "/champions?api_key=" + riotAPIKey, function (data) {
+                        var request = region.host + "/championmastery/location/" + region.platform + "/player/" + player.id + "/champions";
+                        requestJSON(request + "?api_key=" + riotAPIKey, function (data) {
                             var result = {
                                 player: {
                                     icon: ddragon + "img/profileicon/" + player.profileIconId + ".png",
@@ -181,17 +183,15 @@ function getPlayer(req, res) {
                             });
                             updateHighscores(result, player.id, region, player.standardizedName);
                             res.status(200).send(result);
-                        }, function (code) {
-                            res.status(500).send("Error from Riot's API server (" + code + ")");
+                        }, function (response) {
+                            res.status(500).send("Error from Riot's API server (" + response.statusCode + ")");
+                            console.error("Error for API request '%s': %s (%d)", request, response.body, response.statusCode);
                         });
                     } else {
                         res.status(302).send(encodeURIComponent(playerInfo.redirect));
                     }
                 }, function (error) {
                     res.status(error.code).send(error.message);
-                    if (error.details) {
-                        console.error(error.details);
-                    }
                 });
             } else {
                 res.status(400).send("Name contains invalid characters");
@@ -214,7 +214,8 @@ function standardizeName(name) {
 
 function getPlayerInfo(name, region) {
     return new Promise(function (resolve, reject) {
-        requestJSON(region.host + "/api/lol/" + region.region + "/v1.4/summoner/by-name/" + encodeURIComponent(name) + "?api_key=" + riotAPIKey, function (players) {
+        var request = region.host + "/api/lol/" + region.region + "/v1.4/summoner/by-name/" + encodeURIComponent(name);
+        requestJSON(request + "?api_key=" + riotAPIKey, function (players) {
             //There was a point where some regions were returning empty responses with 200 codes instead of 404 codes. This check is kept in case that happens again
             if (Object.keys(players).length > 0) {
                 var standardizedName = Object.keys(players)[0];
@@ -239,15 +240,28 @@ function getPlayerInfo(name, region) {
                     for (var j = 0; j < champion.length; j++) {
                         var score = champion[j];
                         if (standardizedName === score.standardizedName && region.region === score.region) {
-                            requestJSON(region.host + "/api/lol/" + region.region + "/v1.4/summoner/" + score.id + "?api_key=" + riotAPIKey, function (players) {
+                            var fallbackRequest = region.host + "/api/lol/" + region.region + "/v1.4/summoner/" + score.id;
+                            requestJSON(fallbackRequest + "?api_key=" + riotAPIKey, function (players) {
                                 var player = players[Object.keys(players)[0]].name;
+                                if (LOG_HIGHSCORE_CHANGES) {
+                                    console.log("Player %d on %s has had their name updated from '%s' to '%s'", score.id, score.region, score.name, player);
+                                }
                                 resolve({redirect: player});
-                            }, function (code) {
-                                reject({
-                                    code: 500,
-                                    message: "Unknown error",
-                                    details: "Error " + code + " while looking up fallback player ID"
-                                });
+                            }, {
+                                404: function () {
+                                    console.error("Unable to find fallback summoner for ID %d on %s (previously known as %s)", score.id, score.region, score.name);
+                                    reject({
+                                        code: 404,
+                                        message: "Unable to find summoner or fallback summoner"
+                                    });
+                                },
+                                0: function (response) {
+                                    console.error("Error for API request '%s': %s (%d)", fallbackRequest, response.body, response.statusCode);
+                                    reject({
+                                        code: 500,
+                                        message: "Unknown error"
+                                    });
+                                }
                             });
                             return;
                         }
@@ -258,11 +272,11 @@ function getPlayerInfo(name, region) {
                     message: "Summoner not found. Make sure the name and region are correct."
                 });
             },
-            0: function (code) {
+            0: function (response) {
+                console.error("Error for API request '%s': %s (%d)", request, response.body, response.statusCode);
                 reject({
                     code: 500,
-                    message: "Unknown error",
-                    details: "Error " + code + " while looking up player ID"
+                    message: "Unknown error"
                 });
             }
         });
@@ -298,14 +312,19 @@ function updateHighscore(data, summonerId, region, champion, standardizedName) {
                     break;
                 }
             } else {
-                highscores[championId][i] = {
-                    name: data.player.name,
-                    id: summonerId,
-                    region: region.region,
-                    points: champion.points,
-                    standardizedName: standardizedName
-                };
-                console.log("Updated highscore for " + championId + " by " + summonerId + " with " + champion.points + " at " + i);
+                var oldScore = highscores[championId][i];
+                if (oldScore.name !== data.player.name || oldScore.points !== champion.points) {
+                    highscores[championId][i] = {
+                        name: data.player.name,
+                        id: summonerId,
+                        region: region.region,
+                        points: champion.points,
+                        standardizedName: standardizedName
+                    };
+                    if (LOG_HIGHSCORE_CHANGES) {
+                        console.log("Updated highscore. '%s' (%s) now has highscore at index %d for %s with a score of %d", data.player.name, region.region, i, champions[championId].name, champion.points);
+                    }
+                }
                 break;
             }
         } else {
@@ -325,18 +344,20 @@ function setScore(championId, place, name, summonerId, region, points, isNew, st
     };
     highscores[championId].splice(place, 0, score);
     highscores[championId] = highscores[championId].slice(0, HSCOUNT.track);
-    if (isNew) {
-        console.log("Created highscore for " + championId + " by " + score.id + " with " + score.points + " at " + place);
-    } else {
-        console.log("Modified highscore for " + championId + " by " + score.id + " with " + score.points + " at " + place);
+    if (LOG_HIGHSCORE_CHANGES) {
+        if (isNew) {
+            console.log("Created new highscore. '%s' (%s) now has highscore at index %d for %s with a score of %d", name, region, place, champions[championId].name, points);
+        } else {
+            console.log("Modifed highscore. '%s' (%s) now has highscore at index %d for %s with a score of %d", name, region, place, champions[championId].name, points);
+        }
     }
 }
 
 function saveHighscores() {
     fs.writeFile(highscoreDataPath, JSON.stringify(highscores, null, "\t"), function (err) {
         if (err) {
-            console.error("Error saving highscores: " + err);
-        } else {
+            console.error("Error saving highscores: ", err);
+        } else if (LOG_HIGHSCORE_CHANGES) {
             console.log("Saved highscores");
         }
     });
@@ -344,26 +365,27 @@ function saveHighscores() {
 
 function requestJSON(url, success, errors) {
     (url.toLowerCase().indexOf("https://") === 0 ? HTTPS : HTTP).get(url, function (response) {
-        if (response.statusCode === 200) {
-            var body = "";
-            response.on("data", function (segment) {
-                body += segment;
-            });
-            response.on("end", function () {
+        var body = "";
+        response.on("data", function (segment) {
+            body += segment;
+        });
+        response.on("end", function () {
+            if (response.statusCode === 200) {
                 var data = JSON.parse(body);
                 success(data);
-            });
-        } else if (errors) {
-            if (typeof (errors) === "function") {
-                errors(response.statusCode);
-            } else {
-                if (errors[response.statusCode]) {
-                    errors[response.statusCode]();
-                } else if (errors[0]) {
-                    errors[0](response.statusCode);
+            } else if (errors) {
+                response.body = body;
+                if (typeof (errors) === "function") {
+                    errors(response);
+                } else {
+                    if (errors[response.statusCode]) {
+                        errors[response.statusCode](response);
+                    } else if (errors[0]) {
+                        errors[0](response);
+                    }
                 }
             }
-        }
+        });
     });
 }
 
@@ -419,7 +441,7 @@ function start() {
                             for (var j = 0; j < i; j++) {
                                 //no idea what happened, but I fixed it
                                 if (champion[j] && champion[i] && champion[j].id === champion[i].id) {
-                                    console.log(champion[j].id + " has highscores for " + key + " at " + i + " and " + j);
+                                    console.error(champion[j].id, " had highscores for ", key, " at ", i, " and ", j);
                                     highscores[key].splice(i, 1);
                                 }
                             }
@@ -439,14 +461,14 @@ function start() {
                     app.listen(port, ipaddr);
                     console.log("started");
                 } else {
-                    console.error("CRITICAL ERROR LOADING HIGHSCORE DATA (" + err + ")");
+                    console.error("CRITICAL ERROR LOADING HIGHSCORE DATA (", err, ")");
                 }
             });
-        }, function (code) {
-            console.error("CRITICAL ERROR GETTING STATIC CHAMPION DATA (" + code + ")");
+        }, function (response) {
+            console.error("CRITICAL ERROR GETTING STATIC CHAMPION DATA: ", response.body, " (", response.statusCode, ")");
         });
-    }, function (code) {
-        console.error("CRITICAL ERROR GETTING DDRAGON VERSION (" + code + ")");
+    }, function (response) {
+        console.error("CRITICAL ERROR GETTING DDRAGON VERSION: ", response.body, " (", response.statusCode, ")");
     });
 }
 
