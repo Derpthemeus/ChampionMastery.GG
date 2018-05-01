3d000277330f730b350ad4f64353241aed8d1d84
Matthew Oslan <derpthemeus@gmail.com>
2018-05-01 13:29:19 -0400

    Remove Bootstrap/jQuery from champion list

--- public/js/highscores.js
+++ public/js/highscores.js
@@ -1,4 +1,4 @@
-$(document).ready(() => {
+document.addEventListener("DOMContentLoaded", () => {
 	document.getElementById("quickJump").addEventListener("change", (event) => {
 		if (event.target.value !== "quick_jump") {
 			window.location.href = "/champion?champion=" + event.target.value;
