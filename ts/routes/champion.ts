07ae9e5c46ac165180879073d0a22b6f2a3ad94a
Matthew Oslan <derpthemeus@gmail.com>
2018-07-30 14:24:06 -0400

    Cleanup nested if statements in routes

--- ts/routes/champion.ts
+++ ts/routes/champion.ts
@@ -6,23 +6,25 @@ import express = require("express");
 
 export function renderChampion(req: express.Request, res: express.Response): void {
 	const championId: number = +req.query.champion;
-	if (championId) {
-		const champion: Champion = Champion.getChampionById(championId);
-		if (champion) {
-			const scores: Highscore[] = new Array(Config.highscoreCount.display);
-			for (let i = 0; i < Config.highscoreCount.display; i++) {
-				scores[i] = highscores.getHighscore(champion.id, i);
-			}
-
-			res.status(200).render("champion", {
-				...COMMON_DATA,
-				champion: champion,
-				scores: scores
-			});
-		} else {
-			renderError(res, 404, "Champion not found");
-		}
-	} else {
+	if (!championId) {
 		renderError(res, 400, "No champion specified");
+		return;
 	}
+
+	const champion: Champion = Champion.getChampionById(championId);
+	if (!champion) {
+		renderError(res, 404, "Champion not found");
+		return;
+	}
+
+	const scores: Highscore[] = new Array(Config.highscoreCount.display);
+	for (let i = 0; i < Config.highscoreCount.display; i++) {
+		scores[i] = highscores.getHighscore(champion.id, i);
+	}
+
+	res.status(200).render("champion", {
+		...COMMON_DATA,
+		champion: champion,
+		scores: scores
+	});
 }
--- ts/routes/summoner.ts
+++ ts/routes/summoner.ts
@@ -15,121 +15,126 @@ const SUMMONER_NAME_REGEX = XRegExp("^[0-9\\p{L} _\\.]+$");
 const TOKENS_NEEDED = new Map([[5, 2], [6, 3]]);
 
 export async function renderSummoner(req: express.Request, res: express.Response): Promise<void> {
-	if (req.query.summoner && typeof req.query.summoner === "string") {
-		if (req.query.region && typeof req.query.region === "string") {
-			const region: Region = Region.REGIONS.get(req.query.region.toUpperCase());
-			if (region) {
-				if (SUMMONER_NAME_REGEX.test(req.query.summoner)) {
-					try {
-						const summonerInfo: {summoner: Summoner, hasNewName: boolean} = await getSummoner(region, req.query.summoner);
-						const summoner: Summoner = summonerInfo.summoner;
-						if (!summonerInfo.hasNewName) {
-							const masteries: ChampionMasteryInfo[] = await apiHandler.getChampionMasteries(region, summoner.id);
+	if (!req.query.summoner || typeof req.query.summoner !== "string") {
+		renderError(res, 400, "No summoner name specified");
+		return;
+	}
 
-							highscores.updateAllHighscores(masteries, summoner, region);
+	if (!req.query.region || typeof req.query.region !== "string") {
+		renderError(res, 400, "No region specified");
+		return;
+	}
 
-							const champions: ChampionInfo[] = new Array(masteries.length);
+	const region: Region = Region.REGIONS.get(req.query.region.toUpperCase());
+	if (!region) {
+		renderError(res, 400, "Invalid region");
+		return;
+	}
 
-							let totalLevel: number = 0, totalPoints: number = 0, totalChests: number = 0;
-							for (let i = 0; i < masteries.length; i++) {
-								const masteryChampion: ChampionMasteryInfo = masteries[i];
+	if (!SUMMONER_NAME_REGEX.test(req.query.summoner)) {
+		renderError(res, 400, "Name contains invalid characters", "Make sure the summoner name is correct.");
+		return;
+	}
 
-								totalLevel += masteryChampion.championLevel;
-								totalPoints += masteryChampion.championPoints;
-								if (masteryChampion.chestGranted) {
-									totalChests++;
-								}
+	try {
+		const summonerInfo: {summoner: Summoner, hasNewName: boolean} = await getSummoner(region, req.query.summoner);
+		const summoner: Summoner = summonerInfo.summoner;
+		if (summonerInfo.hasNewName) {
+			res.redirect(302, `?summoner=${encodeURIComponent(summoner.standardizedName)}&region=${region.id}`);
+			return;
+		}
 
-								/** The tooltip to display when hovering over the champion progress cell */
-								let tooltip: string;
-								/**
-								 * The number used to sort champions when sorting by progress.
-								 * Levels 1-4 have a sorting value equal to their percentage progression to next level. Levels 5-6 have a sorting value of "L0T", where "L" is the level and "T" is the tokens earned.
-								 */
-								let sortingValue: number;
-								let pointsToNextLevel: number;
+		const masteries: ChampionMasteryInfo[] = await apiHandler.getChampionMasteries(region, summoner.id);
 
-								if (masteryChampion.championLevel < 5) {
-									// The percentage to the next level, rounded to 2 decimal places
-									sortingValue = Math.round(masteryChampion.championPointsSinceLastLevel / (masteryChampion.championPointsSinceLastLevel + masteryChampion.championPointsUntilNextLevel) * 10000) / 100;
-									tooltip = `${masteryChampion.championPointsSinceLastLevel}/${masteryChampion.championPointsSinceLastLevel + masteryChampion.championPointsUntilNextLevel} points (${sortingValue}%)`;
-									pointsToNextLevel = masteryChampion.championPointsUntilNextLevel;
-								} else {
-									sortingValue = (100 * masteryChampion.championLevel) + masteryChampion.tokensEarned;
-									// Using a higher value to keep lvl 5 and above at the end of the ascending sort
-									pointsToNextLevel = 90000 + (100 * masteryChampion.championLevel);
+		highscores.updateAllHighscores(masteries, summoner, region);
 
-									if (masteryChampion.championLevel === 7) {
-										tooltip = "Max level";
-									} else {
-										tooltip = `${masteryChampion.tokensEarned}/${TOKENS_NEEDED.get(masteryChampion.championLevel)} tokens`;
-									}
-								}
+		const champions: ChampionInfo[] = new Array(masteries.length);
 
-								const champion: Champion = Champion.getChampionById(masteryChampion.championId);
-								const info: ChampionInfo = {
-									...masteryChampion,
-									// Call the champion "New Champion" if static data ha not been updated yet
-									championName: champion ? champion.name : `New Champion`,
-									tooltip: tooltip,
-									sortingValue: sortingValue,
-									pointsToNextLevel: pointsToNextLevel
-								};
+		let totalLevel: number = 0, totalPoints: number = 0, totalChests: number = 0;
+		for (let i = 0; i < masteries.length; i++) {
+			const masteryChampion: ChampionMasteryInfo = masteries[i];
 
-								champions[i] = info;
-							}
+			totalLevel += masteryChampion.championLevel;
+			totalPoints += masteryChampion.championPoints;
+			if (masteryChampion.chestGranted) {
+				totalChests++;
+			}
 
-							res.status(200).render("summoner", {
-								...COMMON_DATA,
-								summoner: {
-									icon: summoner.profileIconId,
-									name: summoner.name,
-									id: summoner.id,
-									region: region.id
-								},
-								champions: champions,
-								totals: {
-									level: totalLevel,
-									points: totalPoints,
-									chests: totalChests,
-									champions: champions.length
-								}
-							});
+			/** The tooltip to display when hovering over the champion progress cell */
+			let tooltip: string;
+			/**
+			 * The number used to sort champions when sorting by progress.
+			 * Levels 1-4 have a sorting value equal to their percentage progression to next level. Levels 5-6 have a sorting value of "L0T", where "L" is the level and "T" is the tokens earned.
+			 */
+			let sortingValue: number;
+			let pointsToNextLevel: number;
 
-							interface ChampionInfo extends ChampionMasteryInfo {
-								championName: string;
-								tooltip: string;
-								sortingValue: number;
-								pointsToNextLevel: number;
-							}
-						} else {
-							res.redirect(302, `?summoner=${encodeURIComponent(summoner.standardizedName)}&region=${region.id}`);
-						}
-					} catch (ex) {
-						if (ex instanceof RateLimitError) {
-							renderError(res, 503, "Server overloaded", "Try again later. Retrying immediately will only make the problem worse.");
-						} else if (ex instanceof apiHandler.APIError) {
-							if (ex.statusCode === 404) {
-								renderError(res, 404, "Player not found", "Make sure the summoner name and region are correct.");
-							} else {
-								renderError(res, 500, `API error (${ex.statusCode})`, "Try refreshing the page. If the problem persists, let me know (contact info in site footer).");
-							}
-						} else {
-							console.error(VError.fullStack(new VError(ex, `Error creating page for summoner "${req.query.summoner}" (${req.query.region})`)));
-							renderError(res, 500, "Unknown error", "Please send me a message (contact info in site footer).");
-						}
-					}
-				} else {
-					renderError(res, 400, "Name contains invalid characters", "Make sure the summoner name is correct.");
-				}
+			if (masteryChampion.championLevel < 5) {
+				// The percentage to the next level, rounded to 2 decimal places
+				sortingValue = Math.round(masteryChampion.championPointsSinceLastLevel / (masteryChampion.championPointsSinceLastLevel + masteryChampion.championPointsUntilNextLevel) * 10000) / 100;
+				tooltip = `${masteryChampion.championPointsSinceLastLevel}/${masteryChampion.championPointsSinceLastLevel + masteryChampion.championPointsUntilNextLevel} points (${sortingValue}%)`;
+				pointsToNextLevel = masteryChampion.championPointsUntilNextLevel;
 			} else {
-				renderError(res, 400, "Invalid region");
+				sortingValue = (100 * masteryChampion.championLevel) + masteryChampion.tokensEarned;
+				// Using a higher value to keep lvl 5 and above at the end of the ascending sort
+				pointsToNextLevel = 90000 + (100 * masteryChampion.championLevel);
+
+				if (masteryChampion.championLevel === 7) {
+					tooltip = "Max level";
+				} else {
+					tooltip = `${masteryChampion.tokensEarned}/${TOKENS_NEEDED.get(masteryChampion.championLevel)} tokens`;
+				}
+			}
+
+			const champion: Champion = Champion.getChampionById(masteryChampion.championId);
+			const info: ChampionInfo = {
+				...masteryChampion,
+				// Call the champion "New Champion" if static data ha not been updated yet
+				championName: champion ? champion.name : `New Champion`,
+				tooltip: tooltip,
+				sortingValue: sortingValue,
+				pointsToNextLevel: pointsToNextLevel
+			};
+
+			champions[i] = info;
+		}
+
+		res.status(200).render("summoner", {
+			...COMMON_DATA,
+			summoner: {
+				icon: summoner.profileIconId,
+				name: summoner.name,
+				id: summoner.id,
+				region: region.id
+			},
+			champions: champions,
+			totals: {
+				level: totalLevel,
+				points: totalPoints,
+				chests: totalChests,
+				champions: champions.length
+			}
+		});
+
+		interface ChampionInfo extends ChampionMasteryInfo {
+			championName: string;
+			tooltip: string;
+			sortingValue: number;
+			pointsToNextLevel: number;
+		}
+	} catch (ex) {
+		if (ex instanceof RateLimitError) {
+			renderError(res, 503, "Server overloaded", "Try again later. Retrying immediately will only make the problem worse.");
+		} else if (ex instanceof apiHandler.APIError) {
+			if (ex.statusCode === 404) {
+				renderError(res, 404, "Player not found", "Make sure the summoner name and region are correct.");
+			} else {
+				renderError(res, 500, `API error (${ex.statusCode})`, "Try refreshing the page. If the problem persists, let me know (contact info in site footer).");
 			}
 		} else {
-			renderError(res, 400, "No region specified");
+			console.error(VError.fullStack(new VError(ex, `Error creating page for summoner "${req.query.summoner}" (${req.query.region})`)));
+			renderError(res, 500, "Unknown error", "Please send me a message (contact info in site footer).");
 		}
-	} else {
-		renderError(res, 400, "No summoner name specified");
 	}
 }
 
