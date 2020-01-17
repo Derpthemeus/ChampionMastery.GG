fc377b6a9f4d48f52b62199c0ff20c5e8f89133a
Matthew Oslan <derpthemeus@gmail.com>
2020-01-17 16:03:03 -0500

    Handle Riot API errors in /summonerScores

--- highscores-service/src/main/java/gg/championmastery/highscoresService/api/http/SummonerScoresHandler.java
+++ highscores-service/src/main/java/gg/championmastery/highscoresService/api/http/SummonerScoresHandler.java
@@ -2,6 +2,15 @@ package gg.championmastery.highscoresService.api.http;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableMap;
+import com.merakianalytics.orianna.datapipeline.riotapi.exceptions.BadRequestException;
+import com.merakianalytics.orianna.datapipeline.riotapi.exceptions.ForbiddenException;
+import com.merakianalytics.orianna.datapipeline.riotapi.exceptions.InternalServerErrorException;
+import com.merakianalytics.orianna.datapipeline.riotapi.exceptions.NotFoundException;
+import com.merakianalytics.orianna.datapipeline.riotapi.exceptions.RateLimitExceededException;
+import com.merakianalytics.orianna.datapipeline.riotapi.exceptions.ServiceUnavailableException;
+import com.merakianalytics.orianna.datapipeline.riotapi.exceptions.UnauthorizedException;
+import com.merakianalytics.orianna.datapipeline.riotapi.exceptions.UnsupportedMediaTypeException;
+import com.merakianalytics.orianna.types.common.OriannaException;
 import com.merakianalytics.orianna.types.common.Platform;
 import com.merakianalytics.orianna.types.dto.championmastery.ChampionMasteries;
 import com.merakianalytics.orianna.types.dto.summoner.Summoner;
@@ -12,9 +21,21 @@ import org.eclipse.jetty.server.handler.AbstractHandler;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class SummonerScoresHandler extends AbstractHandler {
+
+	private static final Map<Class<? extends OriannaException>, Integer> errorCodes = new HashMap<Class<? extends OriannaException>, Integer>() {{
+			put(BadRequestException.class, 400);
+			put(ForbiddenException.class, 403);
+			put(InternalServerErrorException.class, 500);
+			put(NotFoundException.class, 404);
+			put(RateLimitExceededException.class, 429);
+			put(ServiceUnavailableException.class, 503);
+			put(UnauthorizedException.class, 403);
+			put(UnsupportedMediaTypeException.class, 415);
+	}};
 
 	private final ObjectMapper mapper;
 
@@ -41,10 +62,16 @@ public class SummonerScoresHandler extends AbstractHandler {
 			return;
 		}
 
-		Summoner summoner = HighscoresService.getOriannaPipeline().get(Summoner.class, ImmutableMap.of(
-				"platform", platform,
-				"name", summonerName
-		));
+		Summoner summoner;
+		try {
+			summoner = HighscoresService.getOriannaPipeline().get(Summoner.class, ImmutableMap.of(
+					"platform", platform,
+					"name", summonerName
+			));
+		} catch (OriannaException ex) {
+			sendOriannaError(ex, response);
+			return;
+		}
 
 		if (summoner == null) {
 			response.setStatus(404);
@@ -53,16 +80,43 @@ public class SummonerScoresHandler extends AbstractHandler {
 			return;
 		}
 
-		ChampionMasteries summonerScores = HighscoresService.getApi().getSummonerScores(summoner);
+		ChampionMasteries summonerScores;
+		try {
+			summonerScores = HighscoresService.getApi().getSummonerScores(summoner);
+		} catch (OriannaException ex) {
+			sendOriannaError(ex, response);
+			return;
+		}
 
 		Map<String, Object> map = ImmutableMap.of(
 				"summoner", summoner,
 				"scores", summonerScores
 		);
 
-
 		response.setStatus(200);
 		response.setContentType("text/json");
 		mapper.writeValue(response.getWriter(), map);
+	}
+
+	/**
+	 * Responds to a request with an error message indicating that an OriannaException was thrown. If the exception was
+	 * thrown because the Riot API returned an error and the HTTP status code of the original error can be determined,
+	 * the status code of the response will be set to the same code. Otherwise, the code will be set to 500.
+	 * @param ex The OriannaException that was caught.
+	 * @param response The response to send the error through.
+	 * @throws IOException Thrown if an error occurs while writing the response.
+	 */
+	private static void sendOriannaError(OriannaException ex, HttpServletResponse response) throws IOException {
+		Integer code = errorCodes.get(ex.getClass());
+
+		if (code == null) {
+			response.setStatus(500);
+			response.setContentType("text/plain");
+			response.getWriter().write(String.format("An Orianna error occurred (%s).", ex.getClass().getName()));
+		} else {
+			response.setStatus(code);
+			response.setContentType("text/plain");
+			response.getWriter().write("A Riot API error occurred.");
+		}
 	}
 }
