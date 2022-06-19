a3c3573fb4cc2212dd7a45e1b22810de4c5fb125
Matthew Oslan <derpthemeus@gmail.com>
2022-06-19 14:30:15 -0700

    Make healthcheck play nicely with wrapped exceptions

--- healthcheck/healthcheck.py
+++ healthcheck/healthcheck.py
@@ -9,7 +9,8 @@ print("Started")
 while True:
     try:
         requests.get("http://highscores_service:8181", timeout=5)
-    except requests.exceptions.ConnectTimeout:
+    # TODO this should be scoped more tightly to only catch timeouts
+    except:
         container = client.containers.get("cmgg-highscores_service-1")
         container.restart()
         print("Restarted highscores service")
