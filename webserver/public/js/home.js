aad6ee2de8a439d95e860b9c2c48ad0023cd1324
Matthew Oslan <derpthemeus@gmail.com>
2023-01-06 16:57:36 -0800

    Replace quick jump with champion filtering

--- webserver/public/js/home.js
+++ webserver/public/js/home.js
@@ -1,7 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-	document.getElementById("quickJump").addEventListener("change", (event) => {
-		if (event.target.value !== "quick_jump") {
-			window.location.href = "/champion?champion=" + event.target.value;
+	document.getElementById("champion-filter").addEventListener("input", (event) => {
+		const filterText = document.getElementById("champion-filter").value;
+
+		const championNames = document.getElementsByClassName("champion-name");
+		const champions = document.getElementsByClassName("champion");
+		for (let i = 0; i < champions.length; i++) {
+			// TODO use same matching behavior as champ select
+			champions[i].style.display = championNames[i].textContent.toLowerCase().includes(filterText.toLowerCase()) ? "flex" : "none";
 		}
 	});
 });
